1ba705e771e3f0b8722a99b9a208d55fc514aa55
bmacmill <bnobile@gmail.com>
2025-06-14 22:58:46 -0400

    boilerplate code
    set up boilerplate code

---
+++ components/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+import RegularButton from './RegularButton'
+
+export default function Form({ handleSubmit }) {
+    return (
+        <form className="wrapper">
+            <RegularButton handleClick={handleSubmit}>
+                Start Game
+            </RegularButton>
+        </form>
+    )
+}
---
+++ components/RegularButton.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function RegularButton({ children, handleClick }) {
+    return (
+        <button
+            className="btn btn--text"
+            onClick={handleClick}
+        >
+            {children}
+        </button>
+    )
+}
